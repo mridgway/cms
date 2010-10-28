fdccbff78dc4ca1d03506be6efd334fd70296e19
Michael Ridgway <mike@mododesigngroup.com>
2010-10-28 15:21:58 -0400

    Fixed user model repository

--- upload/CMS/User/Model/User.php
+++ upload/CMS/User/Model/User.php
@@ -11,7 +11,7 @@ namespace User\Model;
  * @copyright   Copyright (c) 2009-2010 Modo Design Group (http://mododesigngroup.com)
  * @license     http://github.com/modo/cms/blob/master//LICENSE    New BSD License
  *
- * @Entity(repositoryClass="User\Service\User")
+ * @Entity
  * @Table(name="User")
  *
  * @property int $id
