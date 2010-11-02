538495698231b733481544b131c2d4fa42f513fb
Benjamin Wittenberg <ben@mododesigngroup.com>
2010-11-02 16:22:15 -0400

    Added _auth to AbstractService.

--- upload/CMS/Core/Service/AbstractService.php
+++ upload/CMS/Core/Service/AbstractService.php
@@ -13,6 +13,10 @@ namespace Core\Service;
  */
 abstract class AbstractService
 {
+    /**
+     * @var Core\Auth\Auth
+     */
+    protected $_auth;
 
     /**
      * Entity manager
@@ -66,5 +70,15 @@ abstract class AbstractService
         $this->_em = $em;
 
         return $this;
+    }
+
+    public function getAuth()
+    {
+        return $this->_auth;
+    }
+
+    public function setAuth($auth)
+    {
+        $this->_auth = $auth;
     }
 }
